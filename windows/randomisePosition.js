e91b0643023f38eb55367acc4819cf4cb9a654ba
mengjie-liu <mengjie.liu@yale.edu>
2020-04-21 03:40:08 -0400

    update

--- windows/randomisePosition.js
+++ windows/randomisePosition.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 //    console.log(document.getElementById("drag-8").offsetHeight);
     // var height = document.body.scrollHeight;
     var width = document.body.scrollWidth;
-    for(i=1; i<11; i++) {
+    for(i=1; i<18; i++) {
         var offsetW = (document.getElementById("drag-"+i).offsetWidth) + 100;
         // var offsetH = (document.getElementById("drag-"+i).offsetHeight) + 80;
         document.getElementById("drag-"+i).style.left = (Math.random() * (width - offsetW)) + 'px';
