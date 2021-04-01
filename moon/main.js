307e482bec1ae73874fffe5386428481dd8383ac
mengjie-liu <mengjie.liu@yale.edu>
2021-04-01 17:59:22 -0400

    update

---
+++ moon/main.js
@@ -0,0 +1,38 @@
+$(function () {
+  function requestDeviceOrientation() {
+    if (
+      typeof window.DeviceOrientationEvent !== "undefined" &&
+      typeof window.DeviceOrientationEvent.requestPermission === "function"
+    ) {
+      window.DeviceOrientationEvent.requestPermission()
+        .then((permissionState) => {
+          if (permissionState === "granted") {
+            window.addEventListener("deviceorientation", (e) => {
+              let alpha = e.alpha;
+              let beta = e.beta;
+              let gamma = e.gamma;
+              $(".image").ripples({
+                resolution: 250,
+                dropRadius: 20,
+                perturbance: 0.04,
+              });
+              let angle = Math.abs(360 - alpha);
+              if (angle >= 2) {
+                console.log(alpha);
+                $(".image").ripples("drop", 200, 0, 35, 0.5);
+              }
+            });
+          }
+        })
+        .catch(console.error);
+    } else {
+      console.log("not permitted");
+    }
+  }
+
+  $(document).click(function () {
+    requestDeviceOrientation();
+  });
+
+  // $(".image").ripples("drop", 200, 0, 35, 0.5);
+});
